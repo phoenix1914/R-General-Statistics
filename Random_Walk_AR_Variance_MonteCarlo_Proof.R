29f9a5f7734b63ea8a8d61491844e1af38ed9347
phoenix1914 <30312966+phoenix1914@users.noreply.github.com>
2021-01-14 16:09:27 +0100

    Add files via upload

--- Random_Walk_AR_Variance_MonteCarlo_Proof.R
+++ Random_Walk_AR_Variance_MonteCarlo_Proof.R
@@ -1,5 +1,5 @@
 t <- 1000  # The sample size of the model
-n <- 1000  # The repeated iterations we want to take for each observation, to find VAR
+n <- 50# The repeated VARs we want to obtain and average
 
 vars_df <- data.frame(matrix(ncol=n,nrow=(t)))
 {
@@ -7,13 +7,13 @@ start.time <- Sys.time()
 for (x in 1:n){
 
 yt <- 0
-et <- rnorm(t, mean = 0, sd =0.5)  # Generating error terms
+et <- rnorm(t, mean = 0, sd =1)  # Generating error terms
 yt <- et[1] # Initializing the time series
 vars_df[1,x] = yt[1]
 
 #Generating the time series
 for (i in 2:t) {
-  yt[i] <- 0.8 * yt[i-1]+et[i]   # Remove the factor completely if you want Random Walk instead of AR
+  yt[i] <- yt[i-1]+et[i]
   vars_df[(i),x] = yt[i]
 }
 
@@ -28,3 +28,24 @@ time.taken <- end.time - start.time
 time.taken
 apply(vars_df, 1, var)
 plot(apply(vars_df, 1, var))
+
+
+{
+  library(ggplot2)
+  library(reshape2)
+
+  vars_df <- t(vars_df)
+  rownames(vars_df) <- paste("trial", seq(n), sep = "")
+  colnames(vars_df) <- paste("time", seq(t), sep = "")
+  
+  dat <- as.data.frame(vars_df)
+  dat$trial <- rownames(dat)
+  mdat <- melt(dat, id.vars = "trial")
+  mdat$time <- as.numeric(gsub("time", "", mdat$variable))
+  
+  
+  ggplot(mdat, aes(x=time, y=value, group=trial)) +
+    theme_bw() +
+    theme(panel.grid=element_blank()) +
+    geom_line(size=0.2, alpha=0.1)
+}
