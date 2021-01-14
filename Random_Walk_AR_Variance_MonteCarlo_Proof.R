12dceb3b48874959c9a1b9a54479e97c0fe63dea
phoenix1914 <30312966+phoenix1914@users.noreply.github.com>
2021-01-14 15:22:17 +0100

    Add files via upload

---
+++ Random_Walk_AR_Variance_MonteCarlo_Proof.R
@@ -0,0 +1,30 @@
+t <- 1000  # The sample size of the model
+n <- 1000# The repeated VARs we want to obtain and average
+
+vars_df <- data.frame(matrix(ncol=n,nrow=(t)))
+{
+start.time <- Sys.time()
+for (x in 1:n){
+
+yt <- 0
+et <- rnorm(t, mean = 0, sd =0.5)  # Generating error terms
+yt <- et[1] # Initializing the time series
+vars_df[1,x] = yt[1]
+
+#Generating the time series
+for (i in 2:t) {
+  yt[i] <- 0.8 * yt[i-1]+et[i]
+  vars_df[(i),x] = yt[i]
+}
+
+
+}
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+
+}
+
+time.taken
+apply(vars_df, 1, var)
+plot(apply(vars_df, 1, var))
